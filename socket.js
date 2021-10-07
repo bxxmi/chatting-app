f145eb5dce97c7e014d945c9a78fbd314704106f
bxxmi <bmi6638@gmail.com>
2021-10-07 18:43:04 +0900

    feat: emmet message

--- socket.js
+++ socket.js
@@ -1,6 +1,11 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log('A user is connect');
+
+    socket.on('chat message', (msg) => {
+      console.log('msg:' + msg);
+    });
+    
     socket.on('disconnect', () => {
       console.log('disconnected');
     });
