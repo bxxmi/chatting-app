0a434e2bc08f07ecb0ae0849e0de59b25f574304
bxxmi <bmi6638@gmail.com>
2021-10-07 18:52:51 +0900

    feat: broadcasting message

--- socket.js
+++ socket.js
@@ -3,9 +3,10 @@ module.exports = (io) => {
     console.log('A user is connect');
 
     socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
       console.log('msg:' + msg);
     });
-    
+
     socket.on('disconnect', () => {
       console.log('disconnected');
     });
